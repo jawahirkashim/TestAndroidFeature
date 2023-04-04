8a1a922d8d83def3902911ab839cd6569d17b845
jawahir <jawahir.abul2007@gmail.com>
2023-04-04 11:30:28 +0530

    suspending function: task1 and task2 co-operating function.

--- app/src/main/java/com/example/mycoroutines/MainActivity.kt
+++ app/src/main/java/com/example/mycoroutines/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textView:TextView
@@ -13,8 +17,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.textView)
+        CoroutineScope(Dispatchers.Default).launch{
+            task1()
+        }
+        CoroutineScope(Dispatchers.Default).launch {
+            task2()
+        }
     }
 
+
+    suspend fun task1(){
+        Log.d(TAG, "start task1")  // execute seq: 1
+        yield() // suspension point: now thread run if any coroutine is present => task2 coroutine
+        Log.d(TAG, "edn task1")  // execute seq: 3
+    }
+    suspend fun task2(){
+        Log.d(TAG, "start task2")   // execute seq: 2
+        yield() // suspension point:
+        Log.d(TAG, "end task2")   // execute seq: 4
+    }
 
     fun doExecute(view: View) {
         Log.d(TAG, "doExecute: thread: "+Thread.currentThread().name)
