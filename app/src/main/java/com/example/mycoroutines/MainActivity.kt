b058739dbb2967fb7f9e34681b867cc454af4cf4
jawahir <jawahir.abul2007@gmail.com>
2023-04-15 19:00:53 +0530

    removed method. Keep it like init

--- app/src/main/java/com/example/mycoroutines/MainActivity.kt
+++ app/src/main/java/com/example/mycoroutines/MainActivity.kt
@@ -1,52 +1,14 @@
 package com.example.mycoroutines
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.TextView
-import kotlinx.coroutines.*
-import kotlin.concurrent.thread
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView:TextView
-    private val TAG = MainActivity::class.java.simpleName
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.textView)
-    }
-
-
-    fun doExecute(view: View) {
-        CoroutineScope(Dispatchers.IO).launch {
-            Log.d(TAG, "1- doExecute: ${Thread.currentThread().name}")
-            executeLongRunningTask()
-        }
-
-        MainScope().launch(Dispatchers.Main) {
-            Log.i(TAG, "2 - doExecute: ${Thread.currentThread().name}")
-            //executeLongRunningTask()
-        }
-
-        GlobalScope.launch(Dispatchers.Default) {
-            Log.v(TAG, "3 - doExecute: ${Thread.currentThread().name}")
-            executeLongRunningTask()
-        }
-
-    }
-
-    private fun executeLongRunningTask() {
-        Log.d(TAG, "doExecute: thread: "+Thread.currentThread().name)
-
-        for (i in 0..1000_000_000_000L){
-
-        }
-    }
-
-    fun doCounter(view: View) {
-
-        Log.d(TAG, "doCounter: thread: ${Thread.currentThread().name}")
-         textView.text = "${textView.text.toString().toInt()+1}"
     }
 }
